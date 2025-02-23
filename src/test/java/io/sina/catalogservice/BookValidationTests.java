8a930f1bb5c47946b23eb30de89f242ac83176de
sinaaskarnejad <curse_dmc4@yahoo.com>
2025-02-23 22:41:10 +0330

    [enhance]Add Unit tests for validation

---
+++ src/test/java/io/sina/catalogservice/BookValidationTests.java
@@ -0,0 +1,40 @@
+package io.sina.catalogservice;
+
+import io.sina.catalogservice.domain.Book;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BookValidationTests {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUp() {
+        try (ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory()) {
+            validator = validatorFactory.getValidator();
+        }
+    }
+
+    @Test
+    void whenAllFieldsCorrectThenValidationSucceeds() {
+        var book = new Book("1234567890", "Sample Book", "Sina", 12.5);
+        Set<ConstraintViolation<Book>> constraintViolations = validator.validate(book);
+        assertThat(constraintViolations).isEmpty();
+    }
+
+    @Test
+    void whenIsbnDefinedButIncorrectThenValidationFails() {
+        var book = new Book("a234567890", "Sample Book", "Sina", 12.5);
+        Set<ConstraintViolation<Book>> constraintViolations = validator.validate(book);
+        assertThat(constraintViolations).hasSize(1);
+        assertThat(constraintViolations.iterator().next().getMessage()).isEqualTo("The ISBN format must be valid");
+    }
+}
